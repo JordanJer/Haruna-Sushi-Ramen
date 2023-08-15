57da4bc1d7144428be75b4ca30dce1227caa8ec3
JordanJer <141587863+JordanJer@users.noreply.github.com>
2023-08-14 21:00:28 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+const swiper = new Swiper(".swiper-container", {
+    effect: 'fade', // efecto de desvanecimiento
+    autoplay: {
+        delay: 2800,
+        disableOnInteraction: false,
+    },
+});
+
+
+
+
